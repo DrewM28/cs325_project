47ae5139f3a7346f8473056441e74d3f2057e412
Drew Milton <dmgolf28@gmail.com>
2024-09-11 11:21:00 -0500

    changes

--- Project1.py
+++ Project1.py
@@ -1,29 +1,29 @@
 #Drew Milton
 
-#def main():
-
-    #def ReadPrompt(file):   #reads prompts from file
-        #with open(file, "r") as file:
-            #lines = file.readlines()
-            #print(lines)
-
-    #with open("response.txt", "w") as file:
-
-
-
-    #ReadPrompt("prompts.txt")
-
-
-
-
 #reads prompts from prompts.txt file
-with open("prompts.txt", "r") as file:
-    prompts = file.readlines()
-    print(prompts)
+def read_prompts(filepath):
+    with open("prompts.txt", "r") as file:
+        prompts = file.readlines()
+        return prompts
+        
+
+#call the model
 
 
 #pass prompts to the model
 
 
 #get responses and transfer them to a new txt file
+
+
+def main():
+    filepath = "prompts.txt"
+    prompts = read_prompts(filepath)
+    
+    print(prompts)
+
+
+
+if __name__ == "__main__":
+    main()
 
