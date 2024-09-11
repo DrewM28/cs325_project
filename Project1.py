b831f558abc2e142ef3f797ee31252f271cfeb6c
Drew Milton <dmgolf28@gmail.com>
2024-09-11 11:08:27 -0500

    adding files

---
+++ Project1.py
@@ -0,0 +1,29 @@
+#Drew Milton
+
+#def main():
+
+    #def ReadPrompt(file):   #reads prompts from file
+        #with open(file, "r") as file:
+            #lines = file.readlines()
+            #print(lines)
+
+    #with open("response.txt", "w") as file:
+
+
+
+    #ReadPrompt("prompts.txt")
+
+
+
+
+#reads prompts from prompts.txt file
+with open("prompts.txt", "r") as file:
+    prompts = file.readlines()
+    print(prompts)
+
+
+#pass prompts to the model
+
+
+#get responses and transfer them to a new txt file
+
---
+++ README.md
@@ -0,0 +1,5 @@
+Drew Milton
+Project 1
+CS 325
+
+Using phi-3 mini to pass along three prompts and get the responses from the sLLM and put them into a new txt file
