18889549e61497d2089be9bd6172163e4e6d2051
Drew Milton <dmgolf28@gmail.com>
2024-09-24 12:09:27 -0500

    changing file name

--- Project1.py
+++ Project1.py
@@ -1,41 +1,37 @@
-#Drew Milton
-
-#reads prompts from prompts.txt file
-def read_prompts(filepath):
-    with open(filepath, "r") as file:
-        prompts = file.readlines()
-        return prompts
-        
-
-#call the model
+#using hugging face connects to AI using API
+from huggingface_hub import InferenceClient
 
 
-#pass prompts to the model
-# Use a pipeline as a high-level helper
-def pass_prompts():
-    from transformers import pipeline
+#reads the prompts in from the prompts text file
+with open("prompts.txt", "r") as file: #with open, opens prompts.txt and r means to read in the prompts
+    prompts = file.readlines() #file.readlines reads the prompts in the file and stores it in prompts
 
-    messages = [
-        {"content": prompts},
-    ]
-    pipe = pipeline("text-generation", model="microsoft/Phi-3-mini-128k-instruct", trust_remote_code=True)
-    return pipe
+#calls Phi-3 with a token
+#got this from hugging face
+client = InferenceClient(
+    "microsoft/Phi-3-mini-4k-instruct",
+    token="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",
+)
 
+#gets the responses and outputs them in the terminal
+responses = [] #stores responses into a list
+for prompt in prompts[:3]: #shows only the first 3 prompts
 
-#get responses and transfer them to a new txt file
-
-
-def main():
-    filepath = "prompts.txt"
-    prompts = read_prompts(filepath)
-    
-    print(prompts)
-
-    messages = pass_prompts()
-    print(messages)
+    for message in client.chat_completion(
+	    messages=[{"role": "user", "content": prompt}],
+	    max_tokens=100, 
+	    stream=True,
+):
+        #this is what gets the responses from phi-3
+        response_content = message.choices[0].delta.get("content", "")
+        if response_content:
+            responses.append(response_content)
+        #print(message.choices[0].delta.content, end="") #optional print statement to make sure code was working
 
 
 
-if __name__ == "__main__":
-    main()
-
+#sends the responses to the responses text file
+with open("responses.txt", "w") as response_file: #with open, opens the responses.txt file and w means to write to it
+    response = ''.join(responses) #turns the list of responses into a string called response
+    response_file.write(f"Response to Prompt: \n") #these write to the responses.txt file
+    response_file.write(response + "\n\n")
